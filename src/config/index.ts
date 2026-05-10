c2cd6f150f149adbb2467ba885eba45afd02b474
Ayush Bommana <bommanaayush07@gmail.com>
2026-05-10 14:44:19 +0530

    feat: Add the email worker

--- src/config/index.ts
+++ src/config/index.ts
@@ -14,10 +14,14 @@ const serverConfig: ServerConfig = {
 	PORT: Number(process.env.PORT) ?? 3000,
 	REDIS_SERVER_HOST: process.env.REDIS_SERVER_HOST || "localhost",
 	REDIS_SERVER_PORT: Number(process.env.REDIS_SERVER_PORT) || 6379,
-	BULLMQ_MAILER_QUEUE_NAME: process.env.BULLMQ_MAILER_QUEUE_NAME || "queue:mailer",
-	BULLMQ_MAILER_PAYLOAD_NAME: process.env.BULLMQ_MAILER_PAYLOAD_NAME || "payload:mailer",
-	BULLMQ_MAILER_ADD_EMAIL_ATTEMPTS: Number(process.env.BULLMQ_MAILER_ADD_EMAIL_ATTEMPTS) || 3,
-	BULLMQ_MAILER_ADD_EMAIL_DELAY: Number(process.env.BULLMQ_MAILER_ADD_EMAIL_DELAY) || 1000,
+	BULLMQ_MAILER_QUEUE_NAME:
+		process.env.BULLMQ_MAILER_QUEUE_NAME || "queue:mailer",
+	BULLMQ_MAILER_PAYLOAD_NAME:
+		process.env.BULLMQ_MAILER_PAYLOAD_NAME || "payload:mailer",
+	BULLMQ_MAILER_ADD_EMAIL_ATTEMPTS:
+		Number(process.env.BULLMQ_MAILER_ADD_EMAIL_ATTEMPTS) || 3,
+	BULLMQ_MAILER_ADD_EMAIL_DELAY:
+		Number(process.env.BULLMQ_MAILER_ADD_EMAIL_DELAY) || 1000,
 };
 
 export { serverConfig };
--- src/producers/mailer.producer.ts
+++ src/producers/mailer.producer.ts
@@ -1,17 +1,17 @@
 import { mailerQueue } from "../queues/mailer.queue.ts";
 import type { AddEmailDto } from "../dtos/mailer.dto.ts";
 import { logger } from "../config/logger.config.ts";
-import {serverConfig} from "../config/index.ts";
+import { serverConfig } from "../config/index.ts";
 
 const addEmailToQueue = async (payload: AddEmailDto) => {
 	try {
 		await mailerQueue.add(serverConfig.BULLMQ_MAILER_PAYLOAD_NAME, payload, {
-            attempts: serverConfig.BULLMQ_MAILER_ADD_EMAIL_ATTEMPTS,
-            backoff: {
-                type: "exponential",
-                delay: serverConfig.BULLMQ_MAILER_ADD_EMAIL_DELAY
-            }
-        });
+			attempts: serverConfig.BULLMQ_MAILER_ADD_EMAIL_ATTEMPTS,
+			backoff: {
+				type: "exponential",
+				delay: serverConfig.BULLMQ_MAILER_ADD_EMAIL_DELAY,
+			},
+		});
 		logger.info(
 			`Successfully added the email to queue: ${JSON.stringify(payload)}`,
 		);

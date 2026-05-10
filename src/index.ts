e31051405ec9648bd5e6c9c530364fa4ec23b7c4
Ayush Bommana <bommanaayush07@gmail.com>
2026-05-11 02:35:59 +0530

    feat: Add bull mq dashboard

--- src/index.ts
+++ src/index.ts
@@ -7,9 +7,22 @@ import { logger } from "./config/logger.config.ts";
 import { setupEmailWorker } from "./workers/mailer.worker.ts";
 import type { AddEmailDto } from "./dtos/mailer.dto.ts";
 import { addEmailToQueue } from "./producers/mailer.producer.ts";
+import { ExpressAdapter } from "@bull-board/express";
+import { createBullBoard } from "@bull-board/api";
+import { BullMQAdapter } from "@bull-board/api/bullMQAdapter";
+import { mailerQueue } from "./queues/mailer.queue.ts";
 
 // config app
 const app = express();
+
+// config bull mq dashboard
+const serverAdapter = new ExpressAdapter();
+serverAdapter.setBasePath("/admin/queues");
+
+createBullBoard({
+	queues: [new BullMQAdapter(mailerQueue)],
+	serverAdapter: serverAdapter,
+});
 
 // setup global middlewares
 app.use(express.json());
@@ -17,6 +30,7 @@ app.use(attachCorrelationId);
 
 // setup version routes
 app.use("/api/v1", v1Router);
+app.use("/admin/queues", serverAdapter.getRouter());
 
 // setup the error middleware
 app.use(errorHandler);

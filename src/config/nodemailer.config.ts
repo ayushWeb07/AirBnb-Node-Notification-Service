1b847364401ad069c9a09e70de5c5847ac60df11
Ayush Bommana <bommanaayush07@gmail.com>
2026-05-13 14:19:41 +0530

    feat: Send the mail in the mailer worker

--- src/config/nodemailer.config.ts
+++ src/config/nodemailer.config.ts
@@ -4,9 +4,7 @@ import { serverConfig } from "./index.ts";
 
 // Create a transporter object
 const gmailTransporter = nodemailer.createTransport({
-	host: "smtp.gmail.com",
-	port: 587,
-	secure: false,
+	service: "gmail",
 	auth: {
 		user: serverConfig.MAIL_USER_ADDRESS,
 		pass: serverConfig.MAIL_APP_PASSWORD,
--- src/workers/mailer.worker.ts
+++ src/workers/mailer.worker.ts
@@ -2,12 +2,38 @@ import { Worker, Job } from "bullmq";
 import { serverConfig } from "../config/index.ts";
 import { RedisConnection } from "../config/redis.config.ts";
 import { logger } from "../config/logger.config.ts";
+import { renderTemplateContent } from "../templates/templates.handler.ts";
+import { gmailTransporter } from "../config/nodemailer.config.ts";
+import type { AddEmailDto } from "../dtos/mailer.dto.ts";
 
-const setupEmailWorker = () => {
+const setupEmailWorker = async () => {
 	const emailWorker = new Worker(
 		serverConfig.BULLMQ_MAILER_QUEUE_NAME,
 		async (job: Job) => {
-			job.data;
+			const payload: AddEmailDto = job.data;
+
+			// get the email content
+			const emailContent = await renderTemplateContent(
+				payload.templateId,
+				payload.params,
+			);
+
+			// Configure the mail object
+			const mailOptions = {
+				from: serverConfig.MAIL_USER_ADDRESS,
+				to: payload.toMailAddress,
+				subject: payload.subject,
+				text: emailContent,
+			};
+
+			// Send the email
+			gmailTransporter.sendMail(mailOptions, function (error, info) {
+				if (error) {
+					console.log("Error:", error);
+				} else {
+					console.log("Email sent: ", info.response);
+				}
+			});
 
 			// call the ext. email service
 			logger.info(`Processing the email for: ${JSON.stringify(job.data)}`);
